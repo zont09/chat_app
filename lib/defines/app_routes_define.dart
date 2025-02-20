0742e6cfc0ccb80a5276512d5170c59435765fba
zont09 <zontcn09@gmail.com>
2025-02-20 12:18:57 +0700

    first commit

---
+++ lib/defines/app_routes_define.dart
@@ -0,0 +1,18 @@
+import 'package:code/app_texts.dart';
+
+enum AppRoutes { login, chat, overview, profile }
+
+extension AppRoutesExtension on AppRoutes {
+  String get text {
+    switch (this) {
+      case AppRoutes.profile:
+        return AppText.textRouteProfile.text;
+      case AppRoutes.chat:
+        return AppText.textRouteChat.text;
+      case AppRoutes.overview:
+        return AppText.textRouteOverview.text;
+      default:
+        return AppText.textRouteLogin.text;
+    }
+  }
+}
